a0d2de4fed713e26f772d75c30a560226dba2f6c
NiXeon <mixeon@gmail.com>
2022-01-23 02:23:34 +0500

    finilize

--- client/components/dashboardProfile.js
+++ client/components/dashboardProfile.js
@@ -10,7 +10,7 @@ const DashboardProfile = () => {
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
           <div id="title">Profile</div>
-          <div id="title">{user}</div>
+          <div id="username">{user}</div>
           <div>
             <Link to="/dashboard">Go To Root</Link>
           </div>
